7a48450b33124cdec389e4c60417b7438ea26755
Alexandra <alexandraroze2000@gmail.com>
2022-10-08 20:54:05 +0300

    clean_data.py

---
+++ clean_data.py
@@ -0,0 +1,22 @@
+import re
+from nltk.corpus import stopwords
+from pymystem3 import Mystem
+from tqdm import tqdm
+
+
+def clean_data(texts: list[str], verbose: bool = False) -> [str]:
+    stem = Mystem()
+    len_texts = len(texts)
+    russian_stopwords = stopwords.words("russian")
+    clean_texts = [[] for _ in range(len_texts)]
+    if verbose:
+        iterations = tqdm(range(len_texts))
+    else:
+        iterations = range(len_texts)
+    for i in iterations:
+        text = texts[i]
+        lemmas = stem.lemmatize(text)
+        lemmas = [word for word in lemmas if (word not in russian_stopwords) and (word != ' ') and (word != '\n')]
+        clean_lemmas = re.sub('[^A-Za-zА-Яа-я\s]', '', ' '.join(lemmas)).lower()
+        clean_texts[i] = clean_lemmas
+    return clean_texts
