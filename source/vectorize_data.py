8c2a320811524c00ae7e89f9eb3e9cb29e024d59
Aleksandra Sorokovikova <71643875+AleksandraSorokovikova@users.noreply.github.com>
2022-10-09 01:12:47 +0300

    Update vectorize_data.py

--- source/vectorize_data.py
+++ source/vectorize_data.py
@@ -1,9 +1,17 @@
 from typing import List
 import pickle
 import numpy as np
+from tqdm import tqdm
 
 
-def vectorize_data(clean_texts: List[str]) -> np.array:
+def vectorize_data(clean_texts: List[str], verbose: bool = False) -> List[np.array]:
     with open('../models/transformer_model.pickle', 'rb') as f:
         transformer_model = pickle.load(f)
-    return transformer_model.encode(clean_texts)
+    result = []
+    if verbose:
+        iterations = tqdm(clean_texts)
+    else:
+        iterations = clean_texts
+    for text in iterations:
+        result.append(transformer_model.encode(text))
+    return result
