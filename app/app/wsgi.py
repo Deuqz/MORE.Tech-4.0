18f0c06c398bef032924440fef6cda6a318b7f47
spdymashevski <spdymashevski@yandex-team.ru>
2022-10-09 06:13:45 +0300

    added datasets and api

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
+import multiprocessing
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
+def run_schedule():
+    from utils.parse_news import run_parse_all
+    import schedule
+    run_parse_all()
+proc = multiprocessing.Process(group=None, target=run_schedule,
+                               name=None, args=(),
+                               kwargs={}, daemon=None)
+
+proc.start()
